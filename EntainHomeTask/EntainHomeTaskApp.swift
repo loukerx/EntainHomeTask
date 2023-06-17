5f0c4980f7ddcb46f908e1d4f7d70b5fd9591bdd
Yin Hua <loukerx@gmail.com>
2023-06-17 17:13:19 +1000

    Initial Commit

---
+++ EntainHomeTask/EntainHomeTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntainHomeTaskApp.swift
+//  EntainHomeTask
+//
+//  Created by Yin Hua on 17/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct EntainHomeTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
